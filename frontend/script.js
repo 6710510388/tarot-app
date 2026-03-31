fe19d72ee3dcd714c462223fa0fa8e3870d8f3dd
6710510388 <6710510388@psu.ac.th>
2026-03-31 20:47:06 +0700

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,19 @@
-async function drawCard() {
+async function drawCards() {
   const res = await fetch("/api/draw");
   const data = await res.json();
 
-  document.getElementById("result").innerText = data.name;
+  const container = document.getElementById("cards");
+  container.innerHTML = "";
+
+  data.cards.forEach(card => {
+    const div = document.createElement("div");
+    div.className = "card";
+
+    div.innerHTML = `
+      <h3>${card.name}</h3>
+      <p>${card.meaning}</p>
+    `;
+
+    container.appendChild(div);
+  });
 }
